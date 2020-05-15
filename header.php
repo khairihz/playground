990645746b6129fe29bf574d99fbf0b6106e5a47
Kheireddine Hezzi <khairi.hezzi@gmail.com>
2020-05-15 05:12:08 +0100

    Header and Navbar Setup

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
@@ -25,35 +24,51 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'playground' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$playground_description = get_bloginfo( 'description', 'display' );
-			if ( $playground_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $playground_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<!-- HEADER
+	================================================== -->
+	<header class="site-header" role="banner">
+		
+		<!-- NAVBAR
+		================================================== -->
+		<div class="navbar-wrapper">
+			
+			<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				
+				<!-- container -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'playground' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<div class="container">
+
+
+					<!-- navbar-header -->
+
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="/"><img src="assets/img/logo.png" alt="Bootstrap to Wordpress"></a>
+					</div>
+
+					<!-- navbar header -->
+
+					<!-- If the menu 'WP admin area' is not set, then the "menu_class" is applied to "container". In other words, it overwrites the "container_class".
+					Ref : http://wordpress.org/support/topic/wp_nav_menu_menu_class-usage-bug?replies=4 -->
+
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'primary',
+							'container' => 'nav',
+							'container_class' => 'navbar-collapse collapse',
+							'menu_class' => 'nav navbar-nav navbar-right'
+						)); 
+					?>
+				</div>
+				
+				<!-- container -->
+
+			</div>
+		
+		</div>
+	</header>
